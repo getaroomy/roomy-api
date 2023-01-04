c5905b799cdc775461e405ca9f09730ec6bf8a2f
Matthew Gaim <matthewgaim@gmail.com>
2023-01-04 14:43:59 -0800

    Re-add dotenv

--- models/db.py
+++ models/db.py
@@ -1,6 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 certs = {
   'type': os.getenv("API_FIREBASE_TYPE"),
