79b7ef6eb0146fe97b736bfa0dcd5fbdf578f251
Matthew Gaim <matthewgaim@gmail.com>
2023-01-04 16:01:27 -0800

    Breaking down env var's to their own vars

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,8 @@ COPY . ./
 # Install Python Packages
 RUN pip install -r requirements.txt
 
+# Set an environment variable for the port for 8080
+ENV PORT 8080
+
 # Run gunicorn bound to the 8080 port.
-CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --preload main:app
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 main:app
--- models/db.py
+++ models/db.py
@@ -5,17 +5,32 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+firebase_type = os.getenv("API_FIREBASE_TYPE")
+project_id = os.getenv("API_FIREBASE_PROJECT_ID")
+private_key_id = os.getenv("API_FIREBASE_PRIVATE_KEY_ID")
+private_key = os.getenv("API_FIREBASE_PRIVATE_KEY")
+if isinstance(private_key, str):
+  private_key = private_key.replace('\\n', '\n')
+else:
+  print("API_FIREBASE_PRIVATE_KEY is empty!")
+client_email = os.getenv("API_FIREBASE_CLIENT_EMAIL")
+client_id = os.getenv("API_FIREBASE_CLIENT_ID")
+auth_uri = os.getenv("API_FIREBASE_AUTH_URI")
+token_uri = os.getenv("API_FIREBASE_TOKEN_URI")
+auth_provider_x509_cert_url = os.getenv("API_FIREBASE_AUTH_PROVIDER_X509_CERT_URL")
+client_x509_cert_url = os.getenv("API_FIREBASE_CLIENT_X509_CERT_URL")
+
 certs = {
-  'type': os.getenv("API_FIREBASE_TYPE"),
-  'project_id': os.getenv("API_FIREBASE_PROJECT_ID"),
-  'private_key_id': os.getenv("API_FIREBASE_PRIVATE_KEY_ID"),
-  'private_key': os.getenv("API_FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
-  'client_email': os.getenv("API_FIREBASE_CLIENT_EMAIL"),
-  'client_id': os.getenv("API_FIREBASE_CLIENT_ID"),
-  'auth_uri': os.getenv("API_FIREBASE_AUTH_URI"),
-  'token_uri': os.getenv("API_FIREBASE_TOKEN_URI"),
-  'auth_provider_x509_cert_url': os.getenv("API_FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-  'client_x509_cert_url': os.getenv("API_FIREBASE_CLIENT_X509_CERT_URL")
+  'type': firebase_type,
+  'project_id': project_id,
+  'private_key_id': private_key_id,
+  'private_key': private_key,
+  'client_email': client_email,
+  'client_id': client_id,
+  'auth_uri': auth_uri,
+  'token_uri': token_uri,
+  'auth_provider_x509_cert_url': auth_provider_x509_cert_url,
+  'client_x509_cert_url': client_x509_cert_url
 }
 
 cred = credentials.Certificate(certs)
