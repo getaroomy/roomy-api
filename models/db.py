f3e6011ff6c1285fe441ebd1724af788c19faefb
Matthew Gaim <matthewgaim@gmail.com>
2023-01-10 13:20:41 -0800

    Check for every env-var & add cloudbuild.yaml

--- models/db.py
+++ models/db.py
@@ -5,20 +5,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-firebase_type = os.getenv("API_FIREBASE_TYPE")
-project_id = os.getenv("API_FIREBASE_PROJECT_ID")
-private_key_id = os.getenv("API_FIREBASE_PRIVATE_KEY_ID")
-private_key = os.getenv("API_FIREBASE_PRIVATE_KEY")
-if isinstance(private_key, str):
-  private_key = private_key.replace('\\n', '\n')
-else:
-  print("API_FIREBASE_PRIVATE_KEY is empty!")
-client_email = os.getenv("API_FIREBASE_CLIENT_EMAIL")
-client_id = os.getenv("API_FIREBASE_CLIENT_ID")
-auth_uri = os.getenv("API_FIREBASE_AUTH_URI")
-token_uri = os.getenv("API_FIREBASE_TOKEN_URI")
-auth_provider_x509_cert_url = os.getenv("API_FIREBASE_AUTH_PROVIDER_X509_CERT_URL")
-client_x509_cert_url = os.getenv("API_FIREBASE_CLIENT_X509_CERT_URL")
+def get_env(name: str) -> str:
+  env_var = os.getenv(name)
+  if env_var == None:
+    print(f"Environment variable '{name}' is empty")
+    return ''
+  return env_var
+
+firebase_type = get_env("API_FIREBASE_TYPE")
+project_id = get_env("API_FIREBASE_PROJECT_ID")
+private_key_id = get_env("API_FIREBASE_PRIVATE_KEY_ID")
+private_key = get_env("API_FIREBASE_PRIVATE_KEY").replace('\\n', '\n')
+client_email = get_env("API_FIREBASE_CLIENT_EMAIL")
+client_id = get_env("API_FIREBASE_CLIENT_ID")
+auth_uri = get_env("API_FIREBASE_AUTH_URI")
+token_uri = get_env("API_FIREBASE_TOKEN_URI")
+auth_provider_x509_cert_url = get_env("API_FIREBASE_AUTH_PROVIDER_X509_CERT_URL")
+client_x509_cert_url = get_env("API_FIREBASE_CLIENT_X509_CERT_URL")
 
 certs = {
   'type': firebase_type,
