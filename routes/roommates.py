763c6cf5b5c5768f602999ec96aac3a2e34408a1
Matthew Gaim <matthewgaim@gmail.com>
2023-12-28 18:17:13 -0800

    Update /get_roommates to only get DB data not passed through URL

--- routes/roommates.py
+++ routes/roommates.py
@@ -18,26 +18,21 @@ def get_roommates():
         city = request.args.get("city")
         gender = request.args.get("gender")
         roomWithGender = request.args.get("roomWithGender")
-        doIHavePets = request.args.get("doIHavePets", default=False, type=is_it_true)
-        fineWithHavingPets = request.args.get("fineWithHavingPets", default=False, type=is_it_true)
-        doISmoke = request.args.get("doISmoke", default=False, type=is_it_true)
-        fineWithSmokers = request.args.get("fineWithSmokers", default=False, type=is_it_true)
-        if (city == None or
-            gender == None or
-            roomWithGender == None or
-            doIHavePets == None or
-            fineWithHavingPets == None or
-            doISmoke == None or
-            fineWithSmokers == None):
-            userPreferenceMetadata = get_user_preference_metadata(uid)
+        doIHavePets = request.args.get("doIHavePets", default=None, type=is_it_true)
+        fineWithHavingPets = request.args.get("fineWithHavingPets", default=None, type=is_it_true)
+        doISmoke = request.args.get("doISmoke", default=None, type=is_it_true)
+        fineWithSmokers = request.args.get("fineWithSmokers", default=None, type=is_it_true)
+        if (city == None or gender == None or roomWithGender == None or doIHavePets == None or
+            fineWithHavingPets == None or doISmoke == None or fineWithSmokers == None):
+            userPreferenceMetadata = get_user_preference_metadata(uid, city == None, gender == None, roomWithGender == None, doIHavePets == None, fineWithHavingPets == None, doISmoke == None, fineWithSmokers == None)
             if userPreferenceMetadata:
-                city = userPreferenceMetadata.get("city")
-                gender = userPreferenceMetadata.get("gender")
-                roomWithGender = userPreferenceMetadata.get("roomWithGender")
-                doIHavePets = userPreferenceMetadata.get("doIHavePets")
-                fineWithHavingPets = userPreferenceMetadata.get("fineWithHavingPets")
-                doISmoke = userPreferenceMetadata.get("doISmoke")
-                fineWithSmokers = userPreferenceMetadata.get("fineWithSmokers")
+                if city == None: city = userPreferenceMetadata.get("city")
+                if gender == None: gender = userPreferenceMetadata.get("gender")
+                if roomWithGender == None: roomWithGender = userPreferenceMetadata.get("roomWithGender")
+                if doIHavePets == None: doIHavePets = userPreferenceMetadata.get("doIHavePets")
+                if fineWithHavingPets == None: fineWithHavingPets = userPreferenceMetadata.get("fineWithHavingPets")
+                if doISmoke == None: doISmoke = userPreferenceMetadata.get("doISmoke")
+                if fineWithSmokers == None: fineWithSmokers = userPreferenceMetadata.get("fineWithSmokers")
         preferedRoommatesProps = filter_roommates(gender, roomWithGender, doISmoke, fineWithSmokers, doIHavePets, fineWithHavingPets)
 
         query = (
@@ -64,7 +59,9 @@ def get_roommates():
 def is_it_true(value):
     if value == None:
         return None
-    return value.lower() == 'true'
+    if value.lower() == 'true':
+        return True
+    return False
 
 def filter_roommates(gender: str, roomWithGender: str, doISmoke: bool, fineWithSmokers: bool, doIHavePets: bool, fineWithHavingPets: bool) -> dict:
     """
@@ -117,22 +114,23 @@ def filter_roommates(gender: str, roomWithGender: str, doISmoke: bool, fineWithS
         }
         return preferedRoommatesProps
 
-def get_user_preference_metadata(uid: str):
-    print("get_user_preference_metadata")
+def get_user_preference_metadata(uid: str, city: bool, gender: bool, roomWithGender: bool, doIHavePets: bool, fineWithHavingPets: bool, doISmoke: bool, fineWithSmokers: bool):
+    """
+    Only get user data that's not supplied through the URL
+    """
     try:
         raw_profile = db.collection(u'profiles').document(uid).get()
         if raw_profile.exists:
             profile = raw_profile.to_dict()
             prefs = profile.get('preferences')
-            preferences = {
-                'city': profile.get('city'),
-                'gender': profile.get('gender'),
-                'roomWithGender':prefs.get('roomWithGender'),
-                'doIHavePets':prefs.get('doIHavePets'),
-                'fineWithHavingPets':prefs.get('fineWithHavingPets'),
-                'doISmoke':prefs.get('doISmoke'),
-                'fineWithSmokers':prefs.get('fineWithSmokers'),
-            }
+            preferences = {}
+            if city: preferences['city'] = profile.get('city')
+            if gender: preferences['gender'] = profile.get('gender')
+            if roomWithGender: preferences['roomWithGender'] = prefs.get('roomWithGender')
+            if doIHavePets: preferences['doIHavePets'] = prefs.get('doIHavePets')
+            if fineWithHavingPets: preferences['fineWithHavingPets'] = prefs.get('fineWithHavingPets')
+            if doISmoke: preferences['doISmoke'] = prefs.get('doISmoke')
+            if fineWithSmokers: preferences['fineWithSmokers'] = prefs.get('fineWithSmokers')
             return preferences
         return {}
     except:
