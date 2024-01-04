7b39b71d6c5c6624840941f829ee0236ee758607
Matthew Gaim <matthewgaim@gmail.com>
2024-01-03 22:05:57 -0800

    backend for creating feed posts

--- routes/feed.py
+++ routes/feed.py
@@ -2,70 +2,36 @@ import datetime
 from flask import request, jsonify, Blueprint
 from firebase_admin import firestore
 from models.db import db
+from models.wrappers import token_required
 
 feed_bp = Blueprint('feed_bp', __name__)
 
-@feed_bp.route('/get_articles', methods=['POST','GET'])
+@feed_bp.route('/get_feed_posts', methods=['GET'])
+@token_required
 def get_articles():
     """Return all main page posts"""
     try:
-        query = db.collection(u'articles').order_by(u'actor.date', direction=firestore.Query.DESCENDING).limit(10)
-        last_post_id = request.form.get('last_post')
-        if last_post_id:
-            last_snapshot = db.collection(u'articles').document(last_post_id).get()
-            query = query.start_after(last_snapshot)
+        query = db.collection(u'feed_posts').order_by(u'date', direction=firestore.Query.DESCENDING).limit(10)
         results = query.stream()
         posts = []
-        ids = []
-        for res in results:
-            ids.append(res.id)
-            curr_post = res.to_dict()
-            posts.append(curr_post)
-        return jsonify({"ids": ids, "posts": posts}), 200
+        return jsonify({"posts": posts}), 200
     except Exception as e:
         return f"An error occured: {e}", 500
 
-@feed_bp.route('/get_single_article', methods=['POST','GET'])
-def get_single_article():
-    """Request Input: pid (string)
-
-    Return single main page post
-    """
-    try:
-        pid = request.form['pid']
-        curr_post = db.collection(u'articles').document(pid).get()
-        post = curr_post.to_dict()
-        return jsonify({"ids":curr_post.id, "posts":post }), 200
-    except Exception as e:
-        return f"An error occured: {e}", 500
-
-@feed_bp.route('/post_article', methods=['POST'])
-def post_article():
+@feed_bp.route('/create_feed_post', methods=['POST'])
+@token_required
+def create_feed_post():
     """Request Input: article (dictionary)
 
     Posts single main page post
     """
     try:
-        article = request.json
-        article['actor']['date'] = datetime.datetime.utcnow() # UTC time added here to avoid JSON misformatting
-        db.collection(u'articles').add(article)
+        body = request.json
+        if not body.get("city"):
+            uid = body.get("uid")
+            curr_profile = db.collection(u'profiles').document(uid).get()
+            body['city'] = curr_profile.to_dict()[u'city']
+        db.collection(u'feed_posts').add(body)
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An error occured: {e}", 500
-
-@feed_bp.route('/update_article', methods=['POST'])
-def update_article():
-    """Request Input: article_info (dictionary)
-
-    Update existing post on main page
-    """
-    try:
-        info = request.json
-        pid = info['pid']
-        update = info['update']
-        db.collection(u'articles').document(pid).update(update)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An error occured: {e}", 500
-
-
